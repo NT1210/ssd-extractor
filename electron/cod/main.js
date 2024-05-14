1ab743e5398aaccdb80c4d10634ce93fe3e90f95
NT1210 <nao_annms@yahoo.co.jp>
2024-05-14 11:42:55 +0900

    introduced adaptation to old ssd files

--- electron/cod/main.js
+++ electron/cod/main.js
@@ -2,6 +2,7 @@
 
 const dousei = require('./dousei.js')
 const ssd    = require('./ssd.js')
+const oldSsd = require('./ssdOld.js')
 const utils  = require('./util.js')
 const populate = require('./populate.js')
 
@@ -20,33 +21,58 @@ function main(ssdFile, douseiFile, desktopDir) {
             let workbook = XLSX.readFile(ssdFile)
 
             const sheet_name_list = workbook.SheetNames
-            const sheet1          = workbook.Sheets[sheet_name_list[0]] 
+            const sheet1          = workbook.Sheets[sheet_name_list[0]]
 
-            const dateToBeInserted = utils.display_date(sheet1)
+            if(sheet_name_list[0].toLowerCase().includes("ф")) {
+                const dateToBeInserted = utils.old_display_date(sheet1)
 
-            ////////////////////////////preparation to dousei////////////////////////////////
-            EXTRACT_LIST = ssd.extractCodFromSSD(sheet1, dateToBeInserted, EXTRACT_LIST)
+                ////////////////////////////preparation to dousei////////////////////////////////
+                EXTRACT_LIST = oldSsd.extractCodFromOldSSD(sheet1, dateToBeInserted, EXTRACT_LIST)
 
-            let cod_list = utils.sortout_cod(EXTRACT_LIST)
+                let cod_list = utils.sortout_cod(EXTRACT_LIST)
 
-            let matched_list = utils.detect_fv(cod_list)
+                let matched_list = utils.detect_fv(cod_list)
 
-            console.log("50% completed...")
-        
-            ///////////////////////////////dousei file///////////////////////////////////////
-            populate.loadWorkbookViaPopulate(XlsxPopulate, douseiFile).then(data => {
-                let workbook2 = data[0]
-                let sheet2 = data[1]
-                let target_column = utils.find_column_in_dousei(dateToBeInserted, sheet2, XlsxPopulate)
-
-                dousei.writeToDousei(sheet2, target_column, matched_list, workbook2, dateToBeInserted, desktopDir)
-
-                console.log("100% completed...")
+                console.log("50% completed...")
             
+                ///////////////////////////////dousei file///////////////////////////////////////
+                populate.loadWorkbookViaPopulate(XlsxPopulate, douseiFile).then(data => {
+                    let workbook2 = data[0]
+                    let sheet2 = data[1]
+                    let target_column = utils.find_column_in_dousei(dateToBeInserted, sheet2, XlsxPopulate)
+
+                    dousei.writeToDousei(sheet2, target_column, matched_list, workbook2, dateToBeInserted, desktopDir)
+
+                    console.log("100% completed...")            
+                })
                 
-            })
+                return "ファイル「updated-2024ロシア極東マダラ生産状況.xlsx」をデスクトップに保存しました。"
+
+            }else{
+                const dateToBeInserted = utils.display_date(sheet1)
+
+                ////////////////////////////preparation to dousei////////////////////////////////
+                EXTRACT_LIST = ssd.extractCodFromSSD(sheet1, dateToBeInserted, EXTRACT_LIST)
+
+                let cod_list = utils.sortout_cod(EXTRACT_LIST)
+
+                let matched_list = utils.detect_fv(cod_list)
+
+                console.log("50% completed...")
             
-            return "ファイル「updated-2024ロシア極東マダラ生産状況.xlsx」をデスクトップに保存しました。"
+                ///////////////////////////////dousei file///////////////////////////////////////
+                populate.loadWorkbookViaPopulate(XlsxPopulate, douseiFile).then(data => {
+                    let workbook2 = data[0]
+                    let sheet2 = data[1]
+                    let target_column = utils.find_column_in_dousei(dateToBeInserted, sheet2, XlsxPopulate)
+
+                    dousei.writeToDousei(sheet2, target_column, matched_list, workbook2, dateToBeInserted, desktopDir)
+
+                    console.log("100% completed...")                    
+                })
+                
+                return "ファイル「updated-2024ロシア極東マダラ生産状況.xlsx」をデスクトップに保存しました。"
+            }
         }catch(e){
             return e
         }
---
+++ electron/cod/ssdOld.js
@@ -0,0 +1,110 @@
+let global_fv_cell 
+
+function extractCodFromOldSSD (sheet1, dateToBeInserted, EXTRACT_LIST) {
+    // Extract all data from SSD file
+    for(let i = 8; i < 2000; i++){ 
+        let f_ground_cell = 'A'+i 
+        // let producer_cell = 'A'+i
+        let f_vessel_cell = 'A'+i
+        let product_cell = 'G'+i
+        let qtty_cell = 'J'+i
+        let vessel_type_cell = "B"+i
+        let obj = {}
+        obj["date"]=dateToBeInserted
+        
+        try {
+            let product = sheet1[product_cell].v
+            obj["product"]=product
+            
+        } catch(e) { }
+        
+        if (obj.product){   //if product isn't in the obj dict, then skip the procedure below
+            try {
+                let itogo_row = 0
+                for(let j=1; j<300; j++){
+                    let rev_count = i-j
+                    let rev_f_ground_cell = 'A'+rev_count
+                    if(sheet1[rev_f_ground_cell] === undefined){
+                       
+                    }else{
+                        if(sheet1[rev_f_ground_cell].v.toLowerCase().includes("итого") || sheet1[rev_f_ground_cell].v.toLowerCase().includes("название")){
+                            itogo_row = rev_count
+                            break
+                        }
+                    }
+                }
+
+                // let start_cell = 'A'+itogo_row
+                for(let j=1; j<300; j++){
+                    let rev_count = itogo_row+j
+                    let rev_f_ground_cell = 'A'+rev_count
+
+                    if(sheet1[rev_f_ground_cell] === undefined){
+                      
+                    }else{
+                        obj["f_ground"] = sheet1[rev_f_ground_cell].v
+                        break
+                    }
+
+                }} catch(e){}
+
+            try {
+                let f_vessel = sheet1[f_vessel_cell].v
+                let vessel_type = sheet1[vessel_type_cell].v
+                if(vessel_type){
+                    obj["f_vessel"]=f_vessel
+                    global_fv_cell = 'A'+ (i+1)
+                }
+               
+            } catch(e) { 
+                for(let j=1; j<500; j++){
+                    let rev_f_vessel = ""
+                    let rev_count = i-j
+                    let rev_f_vessel_cell = 'A'+rev_count
+                    let rev_vessel_type_cell = 'B'+rev_count
+                    try{
+                        
+                        let vessel_type = sheet1[rev_vessel_type_cell].v
+                        if(vessel_type){
+                            rev_f_vessel = sheet1[rev_f_vessel_cell].v
+                            obj["f_vessel"]=rev_f_vessel
+                            global_fv_cell = 'A'+ (rev_count+1)
+                        }
+                    }catch(e){
+        
+                    }finally{
+                        if(rev_f_vessel){
+                            break
+                        }
+                    }
+                }
+            }
+        
+            try {
+                obj["producer"] = sheet1[global_fv_cell].v               
+            } catch(e) { 
+                console.log(e)
+            }
+        
+
+        
+            try {
+                let qtty = sheet1[qtty_cell].v
+                obj["qtty"]=qtty
+            } catch(e) { }
+
+        }
+    
+
+        if(obj.product){
+            EXTRACT_LIST.push(obj)
+    
+        }
+    }
+
+    return EXTRACT_LIST
+}
+
+
+
+exports.extractCodFromOldSSD = extractCodFromOldSSD
